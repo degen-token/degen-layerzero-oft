4e41d6a831e55b85c0c23bbf4413d675c8f51593
Jacek <jacek@jtrocinski.com>
2024-05-11 07:04:59 +0200

    Refactor config.

--- layerzero.config.ts
+++ layerzero.config.ts
@@ -42,38 +42,158 @@ const config: OAppOmniGraphHardhat = {
         {
             from: baseContract,
             to: ethereumContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+            },
         },
         {
             from: baseContract,
             to: arbitrumContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+            },
         },
         {
             from: baseContract,
             to: degenContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+                sendConfig: {
+                    ulnConfig: {
+                        requiredDVNs: ['0x9e059a54699a285714207b43B055483E78FAac25'],
+                    },
+                },
+                receiveConfig: {
+                    ulnConfig: {
+                        requiredDVNs: ['0x9e059a54699a285714207b43B055483E78FAac25'],
+                    },
+                },
+            },
         },
         {
             from: ethereumContract,
             to: baseContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+            },
         },
         {
             from: ethereumContract,
             to: arbitrumContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+            },
         },
         {
             from: ethereumContract,
             to: degenContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+                sendConfig: {
+                    ulnConfig: {
+                        requiredDVNs: ['0x589dEDbD617e0CBcB916A9223F4d1300c294236b'],
+                    },
+                },
+                receiveConfig: {
+                    ulnConfig: {
+                        requiredDVNs: ['0x589dEDbD617e0CBcB916A9223F4d1300c294236b'],
+                    },
+                },
+            },
         },
         {
             from: arbitrumContract,
             to: baseContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+            },
         },
         {
             from: arbitrumContract,
             to: ethereumContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+            },
         },
         {
             from: arbitrumContract,
             to: degenContract,
+            config: {
+                enforcedOptions: [
+                    {
+                        msgType: 1,
+                        optionType: ExecutorOptionType.LZ_RECEIVE,
+                        gas: 200000,
+                        value: 0,
+                    },
+                ],
+                sendConfig: {
+                    ulnConfig: {
+                        requiredDVNs: ['0x2f55C492897526677C5B68fb199ea31E2c126416'],
+                    },
+                },
+                receiveConfig: {
+                    ulnConfig: {
+                        requiredDVNs: ['0x2f55C492897526677C5B68fb199ea31E2c126416'],
+                    },
+                },
+            },
         },
         {
             from: degenContract,
@@ -88,23 +208,13 @@ const config: OAppOmniGraphHardhat = {
                     },
                 ],
                 sendConfig: {
-                    executorConfig: {
-                        maxMessageSize: 99,
-                        executor: '0x0000000000000000000000000000000000000000',
-                    },
                     ulnConfig: {
-                        confirmations: BigInt(42),
                         requiredDVNs: ['0x6788f52439ACA6BFF597d3eeC2DC9a44B8FEE842'],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
                     },
                 },
                 receiveConfig: {
                     ulnConfig: {
-                        confirmations: BigInt(42),
                         requiredDVNs: ['0x6788f52439ACA6BFF597d3eeC2DC9a44B8FEE842'],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
                     },
                 },
             },
@@ -122,23 +232,13 @@ const config: OAppOmniGraphHardhat = {
                     },
                 ],
                 sendConfig: {
-                    executorConfig: {
-                        maxMessageSize: 99,
-                        executor: '0x0000000000000000000000000000000000000000',
-                    },
                     ulnConfig: {
-                        confirmations: BigInt(42),
                         requiredDVNs: ['0x6788f52439ACA6BFF597d3eeC2DC9a44B8FEE842'],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
                     },
                 },
                 receiveConfig: {
                     ulnConfig: {
-                        confirmations: BigInt(42),
                         requiredDVNs: ['0x6788f52439ACA6BFF597d3eeC2DC9a44B8FEE842'],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
                     },
                 },
             },
@@ -156,23 +256,13 @@ const config: OAppOmniGraphHardhat = {
                     },
                 ],
                 sendConfig: {
-                    executorConfig: {
-                        maxMessageSize: 99,
-                        executor: '0x0000000000000000000000000000000000000000',
-                    },
                     ulnConfig: {
-                        confirmations: BigInt(42),
                         requiredDVNs: ['0x6788f52439ACA6BFF597d3eeC2DC9a44B8FEE842'],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
                     },
                 },
                 receiveConfig: {
                     ulnConfig: {
-                        confirmations: BigInt(42),
                         requiredDVNs: ['0x6788f52439ACA6BFF597d3eeC2DC9a44B8FEE842'],
-                        optionalDVNs: [],
-                        optionalDVNThreshold: 0,
                     },
                 },
             },
